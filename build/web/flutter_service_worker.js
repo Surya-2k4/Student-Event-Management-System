2584e74620adaf2072deb045dcb5209e67eea2e8
Surya-2k4 <suryanatarajan04@gmail.com>
2026-03-14 11:10:25 +0530

    Updated logics

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "76a5398e62844fa6073ccee5866172f1
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0950a9dc479cd6a4a1315c188eaa3b0c",
+"flutter_bootstrap.js": "7bae935584400370e487c78033e98896",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
